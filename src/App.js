b0e7c907d3fe5cb470e7b4a8135b2a7e01c6d8f7
localadmin <localadmin@localadmins-iMac-2.local>
2017-08-04 13:59:00 -0500

    adjust chess state

--- src/App.js
+++ src/App.js
@@ -12,25 +12,25 @@ class App extends Component {
   }
 
   get currentBoard() {
-    return(
-      this.state.chessBoard.map((row, rowIndex) => {
-        let eachRow = row.map((square, columnIndex) => {
-          let offset = ""
-          if (columnIndex === 0) {
-            offset = " col-xs-offset-2"
-          }
-          return(<div key={columnIndex} className={`col-xs-1${offset}`}>square</div>)
-        })
-        return(<div key={rowIndex} className="row">{eachRow}</div>)
-      })
-    )
+    // return(
+    //   this.state.chessBoard.map((row, rowIndex) => {
+    //     let eachRow = row.map((square, columnIndex) => {
+    //       let offset = ""
+    //       if (columnIndex === 0) {
+    //         offset = " col-xs-offset-2"
+    //       }
+    //       return(<div key={columnIndex} className={`col-xs-1${offset}`}>square</div>)
+    //     })
+    //     return(<div key={rowIndex} className="row">{eachRow}</div>)
+    //   })
+    // )
   }
 
   render() {
     return (
       <div className="App container-fluid">
         <div id="chessBoard">
-          {this.currentBoard}
+          // {this.currentBoard}
         </div>
       </div>
     )

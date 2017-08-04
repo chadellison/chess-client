803a3d05a1cca655ad87d03a713056a4b327265c
Chad Ellison <ChadEllison@Erins-iMac.local>
2017-08-04 17:13:33 -0600

    add color

--- src/App.js
+++ src/App.js
@@ -12,9 +12,15 @@ class App extends Component {
   }
 
   get currentBoard() {
+    let color = false
     return this.boardRows().map((row, rowIndex) => {
       let eachRow = row.map((square, columnIndex) => {
-        return <div key={columnIndex} className={`col-xs-1${this.setOffset(columnIndex)} square`}>square</div>
+        return(
+          <div key={columnIndex}
+            className={`col-xs-1${this.setOffset(columnIndex)} square ${this.squareColor(square.id)}`}>
+            square
+          </div>
+        )
       })
       return(<div key={rowIndex} className="row">{eachRow}</div>)
     })
@@ -34,6 +40,22 @@ class App extends Component {
         offset = " col-xs-offset-2"
     }
     return offset
+  }
+
+  squareColor(id) {
+    let letterKey = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8}
+    let sum = id.split('').reduce((value, character) => {
+      if(letterKey[character]) {
+        character = letterKey[character]
+      }
+      return value + parseInt(character)
+    }, 0)
+
+    if(sum % 2 === 0) {
+      return 'white'
+    } else {
+      return 'black'
+    }
   }
 
   render() {

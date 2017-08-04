fde69b5af60f7252daaa6ae52fdaa5a01802151f
localadmin <localadmin@localadmins-iMac-2.local>
2017-08-04 13:02:39 -0500

    break rows into separate columns

--- src/App.js
+++ src/App.js
@@ -11,10 +11,26 @@ class App extends Component {
     }
   }
 
+  get currentBoard() {
+    return(
+      this.state.chessBoard.map((row, rowIndex) => {
+        let eachRow = row.map((square, columnIndex) => {
+          let offset = ""
+          if (columnIndex === 0) {
+            offset = " col-xs-offset-2"
+          }
+          return(<div key={columnIndex} className={`col-xs-1${offset}`}>square</div>)
+        })
+        return(<div key={rowIndex} className="row">{eachRow}</div>)
+      })
+    )
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className="App container-fluid">
         <div id="chessBoard">
+          {this.currentBoard}
         </div>
       </div>
     )

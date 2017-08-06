d8c4b8209a40b76353849c878a16f665c4272b22
Chad Ellison <ChadEllison@Erins-iMac.local>
2017-08-05 20:23:03 -0600

    refactor

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,17 +1,11 @@
 import React, { Component } from 'react'
 import '../styles/Board.css'
-import jsonChessBoard from '../jsonChessBoard'
 import Square from './Square'
 
 const LETTER_KEY = {"a": 1, "b": 2, "c": 3, "d": 4, "e": 5, "f": 6, "g": 7, "h": 8}
 
 class Board extends Component {
-  constructor() {
-    super()
-  }
-
   get currentSetup() {
-    let color = false
     return this.boardRows().map((row, rowIndex) => {
       let eachRow = row.map((square, columnIndex) => {
         return(

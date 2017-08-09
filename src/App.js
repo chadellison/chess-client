e8e90737165e10270b15f9b233755354a8c3aa39
Andrew Christ and Chad Ellison <chad.ellison0123@gmail.com>
2017-08-09 11:18:04 -0500

    user can login

--- src/App.js
+++ src/App.js
@@ -12,14 +12,18 @@ class App extends Component {
             chessBoard: jsonChessBoard,
             selected: null,
             signUpFormActive: false,
+            signInFormActive: false,
             email: '',
-            password: ''
+            password: '',
+            token: '',
+            loggedIn: ''
         }
         this.handleSelected = this.handleSelected.bind(this)
-        this.handleSignUpForm = this.handleSignUpForm.bind(this)
+        this.handleCredentialForm = this.handleCredentialForm.bind(this)
         this.handleUserEmail = this.handleUserEmail.bind(this)
         this.handleUserPassword = this.handleUserPassword.bind(this)
-        this.handleCreateUser = this.handleCreateUser.bind(this)
+        this.handleUserSubmit = this.handleUserSubmit.bind(this)
+        this.handleLogout = this.handleLogout.bind(this)
         this.move = this.move.bind(this)
     }
 
@@ -37,21 +41,33 @@ class App extends Component {
         })
     }
 
-    handleCreateUser() {
-
+    handleUserSubmit() {
+        let url
+        if(this.state.signUpFormActive) {
+            url = 'http://localhost:8080/api/users'
+        } else {
+            url = 'http://localhost:8080/api/authentication'
+        }
         let body = JSON.stringify({email: this.state.email, password: this.state.password})
-        fetch('http://localhost:8080/api/users',
+        let headers = {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json'
+        }
+
+        fetch(url,
             {
                 method: "POST",
-                headers: {
-                    'Accept': 'application/json, text/plain, */*',
-                    'Content-Type': 'application/json'
-                },
+                headers: headers,
                 body: body
             })
-            .then(this.setState({
+            .then(response => response.json())
+            .then(responseJson => this.setState({
+                token: responseJson.token,
                 email: '',
-                password: ''
+                password: '',
+                signInFormActive: false,
+                signUpFormActive: false,
+                loggedIn: responseJson.email
             }))
     }
 
@@ -67,9 +83,30 @@ class App extends Component {
         }
     }
 
-    handleSignUpForm() {
+    handleCredentialForm(event) {
+        if(event.target.textContent === 'Sign Up') {
+            this.setState({
+                signUpFormActive: !this.state.signUpFormActive
+            })
+
+        } else {
+            this.setState({
+                signInFormActive: !this.state.signInFormActive
+            })
+        }
+        if(event.target.textContent === 'Cancel') {
+            this.setState({
+                signInFormActive: false,
+                signUpFormActive: false
+            })
+        }
+
+    }
+
+    handleLogout() {
         this.setState({
-            signUpFormActive: !this.state.signUpFormActive
+            token: '',
+            loggedIn: '',
         })
     }
 
@@ -82,12 +119,15 @@ class App extends Component {
     }
 
     get signUpForm() {
-        if (this.state.signUpFormActive) {
+        if (this.state.signUpFormActive || this.state.signInFormActive) {
             return <SignUpForm
                 handleUserEmail={this.handleUserEmail}
                 handleUserPassword={this.handleUserPassword}
-                handleSignUpForm={this.handleSignUpForm}
-                handleCreateUser={this.handleCreateUser}
+                handleCredentialForm={this.handleCredentialForm}
+                userEmail={this.state.email}
+                userPassword={this.state.password}
+                handleUserSubmit={this.handleUserSubmit}
+                signInFormActive={this.state.signInFormActive}
             />
         } else {
             return null
@@ -95,10 +135,27 @@ class App extends Component {
 
     }
 
+    get buttons() {
+        let buttons
+        if(this.state.signUpFormActive || this.state.signInFormActive) {
+            buttons = null
+        } else if(this.state.loggedIn !== '') {
+            buttons = <button onClick={this.handleLogout}>Logout</button>
+        } else {
+            buttons = (
+                <div>
+                    <button onClick={this.handleCredentialForm}>Sign In</button>
+                    <button onClick={this.handleCredentialForm}>Sign Up</button>
+                </div>
+            )
+        }
+        return buttons
+    }
+
     render() {
         return (
             <div className='App container-fluid'>
-                <button onClick={this.handleSignUpForm}>Sign Up</button>
+                {this.buttons}
                 {this.signUpForm}
                 <Board chessBoard={this.state.chessBoard}
                        handleSelected={this.handleSelected}
--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -2,15 +2,23 @@ import React, { Component } from "react"
 import "../styles/SignUpForm.css"
 
 export default class SignUpForm extends Component {
+    get buttonName() {
+        if(this.props.signInFormActive) {
+            return 'Sign In'
+        } else {
+            return 'Sign Up'
+        }
+    }
+
     render() {
         return (
             <div>
                 <h4 >Email</h4>
-                <input onChange={this.props.handleUserEmail}></input>
+                <input onChange={this.props.handleUserEmail} value={this.props.userEmail}></input>
                 <h4>Password</h4>
-                <input type="password" onChange={this.props.handleUserPassword}></input>
-                <button onClick={this.props.handleCreateUser}>Sign Up</button>
-                <button onClick={this.props.handleSignUpForm}>Cancel</button>
+                <input type="password" onChange={this.props.handleUserPassword} value={this.props.userPassword}></input>
+                <button onClick={this.props.handleUserSubmit}>{this.buttonName}</button>
+                <button onClick={this.props.handleCredentialForm}>Cancel</button>
             </div>
         )
     }

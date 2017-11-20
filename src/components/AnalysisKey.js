33e127f55bf2821f643cb9e9b2f12a911fa466a7
Chad Ellison <ChadEllison@Erins-iMac.local>
2017-11-19 19:12:12 -0700

    display percentages instead of game count

--- src/components/AnalysisKey.js
+++ src/components/AnalysisKey.js
@@ -3,21 +3,40 @@ import '../styles/AnalysisKey.css'
 import { connect } from 'react-redux'
 
 class AnalysisKey extends Component {
+  winPercentage(color) {
+    let whiteWins = parseInt(this.props.chartData[0].value, 10)
+    let blackWins = parseInt(this.props.chartData[1].value, 10)
+    let draws = parseInt(this.props.chartData[2].value, 10)
+    let totalGames = whiteWins + blackWins + draws
+
+    if (totalGames === 0) {
+      return '0%'
+    } else {
+      if (color === 'white') {
+        return `${Math.round((whiteWins / totalGames) * 100)}%`
+      } else if(color === 'black') {
+        return `${Math.round((blackWins / totalGames) * 100)}%`
+      } else {
+        return `${Math.round((draws / totalGames) * 100)}%`
+      }
+    }
+  }
+
   render() {
     return(
       <div>
         <div className='analyticsKey container-fluid'>
           <div className='row analyticsKey'>
             <div className='whiteWinPercentage col-xs-1'></div>
-            <p className='col-xs-2 analyticsText'>{`White Wins: ${this.props.chartData[0].value}`}</p>
+            <p className='col-xs-2 analyticsText'>{`White Wins: ${this.winPercentage('white')}`}</p>
           </div>
           <div className='row analyticsKey'>
             <div className='blackWinPercentage col-xs-1'></div>
-            <p className='col-xs-2 analyticsText'>{`Black Wins: ${this.props.chartData[1].value}`}</p>
+            <p className='col-xs-2 analyticsText'>{`Black Wins: ${this.winPercentage('black')}`}</p>
           </div>
           <div className='row analyticsKey'>
             <div className='drawPercentage col-xs-1'></div>
-            <p className='col-xs-2 analyticsText'>{`Draws: ${this.props.chartData[2].value}`}</p>
+            <p className='col-xs-2 analyticsText'>{`Draws: ${this.winPercentage('draw')}`}</p>
           </div>
         </div>
         <button className='analyticsButton' onClick={this.props.handleAnalyticsChart}>Hide Analytics</button>
